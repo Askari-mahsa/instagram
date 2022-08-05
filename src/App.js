f2280c8597842daa10d2c8c6c1c3a0619e309803
mahsa <mahsa.ba.askari@gmail.com>
2022-08-05 10:41:06 -0400

    14-5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import "./assets/styles/App.css";
-import { Routes, Route } from "react-router-dom";
-import Login from "pages/Login/Login";
+// import "./assets/styles/App.css";
+// import { Routes, Route } from "react-router-dom";
+// import Login from "pages/Login/Login";
 import SignUp from "pages/SignUp/SignUp";
-import SignUpEmail from "pages/SignUp/SignUpEmail";
-import PreLogin from "./pages/PreLogin/PreLogin";
-import Profile from "pages/Profile/Profile";
-import Home from "./pages/Home/Home";
-import Chat from "pages/Chat/Chat";
-import Explorer from "./pages/Explore/Explorer";
-import Notification from "pages/Notification/Notification";
+// import SignUpEmail from "pages/SignUp/SignUpEmail";
+// import PreLogin from "./pages/PreLogin/PreLogin";
+// import Profile from "pages/Profile/Profile";
+// import Home from "./pages/Home/Home";
+// import Chat from "pages/Chat/Chat";
+// import Explorer from "./pages/Explore/Explorer";
+// import Notification from "pages/Notification/Notification";
 
 const App = () => {
 	return (
 		<>
-			<Routes>
+			<SignUp />
+			{/* <Routes>
 				<Route path="/" element={<PreLogin />} />
 				<Route path="/SignUp" element={<SignUp />} />
 				<Route path="/Login" element={<Login />}></Route>
@@ -23,7 +24,7 @@ const App = () => {
 				<Route path="/chat" element={<Chat />}></Route>
 				<Route path="/explorer" element={<Explorer />}></Route>
 				<Route path="/home" element={<Home />}></Route>
-			</Routes>
+			</Routes> */}
 		</>
 	);
 };
